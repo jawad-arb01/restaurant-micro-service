85857708337151ed6763a6249119df325dd807e0
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-11-30 23:00:43 +0100

    create the micro service architecture

---
+++ BACK-END/repas-service/src/main/java/com/arbahi/repasservice/RepasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arbahi.repasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasServiceApplication.class, args);
+    }
+
+}
---
+++ BACK-END/repas-service/src/test/java/com/arbahi/repasservice/RepasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbahi.repasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
