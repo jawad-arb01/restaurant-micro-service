1203c25b0c2c363acbb7e3c92028c1f2ad77442b
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-12-04 17:06:33 +0100

    update the delete method in the client micro Service

--- BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
+++ BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
@@ -46,7 +46,7 @@ public class ClientController {
         return clientService.getAllMeals();
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public void deleteClient(@PathVariable Long id) {
         clientService.deleteClient(id);
     }
