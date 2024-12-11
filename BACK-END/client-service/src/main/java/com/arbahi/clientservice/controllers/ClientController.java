0c37ab5357493a8403a8905ab0a01fbaa367433f
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-12-11 12:11:51 +0100

    update the end point for get all clients

--- BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
+++ BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
@@ -26,7 +26,7 @@ public class ClientController {
         return clientService.getClientById(id);
     }
 
-    @GetMapping
+    @GetMapping("/getall")
     public List<Client> getAllClients() {
         return clientService.getAllClients();
     }
