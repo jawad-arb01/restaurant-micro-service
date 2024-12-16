4a742a4b7e97506629889d8e829e33d135661910
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-12-16 15:06:31 +0100

    update in the client controller cross origin

--- BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
+++ BACK-END/client-service/src/main/java/com/arbahi/clientservice/controllers/ClientController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/clients")
+@CrossOrigin
 public class ClientController {
 
     private final ClientService clientService;
